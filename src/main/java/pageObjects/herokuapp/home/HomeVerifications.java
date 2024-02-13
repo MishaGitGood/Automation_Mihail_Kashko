f64775142fc99d8edcac7c22bb748f5c6c0f536c
Misha <m.kashko.21vek.by@gmail.com>
2024-02-13 23:32:42 +0300

    add lecture11 and add .allure & .idea add to .gitignore

---
+++ src/main/java/pageObjects/herokuapp/home/HomeVerifications.java
@@ -0,0 +1,17 @@
+package pageObjects.herokuapp.home;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class HomeVerifications{
+
+    private final HomePage homePage;
+
+    public HomeVerifications(HomePage homePage){
+        this.homePage = homePage;
+    }
+
+    public void verifyPageTitle(){
+        homePage.wait.until(ExpectedConditions.textToBe(homePage.title, "File Downloader"));
+    }
+
+}
