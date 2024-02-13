3df3eabf5c778187312b7c9927e654dbaed94d6a
Misha <m.kashko.21vek.by@gmail.com>
2024-02-13 23:34:29 +0300

    add lecture11 and add .allure & .idea add to .gitignore, and part lecture 12

---
+++ src/main/java/pageObjects/herokuapp/SortableDataTablesPage.java
@@ -0,0 +1,6 @@
+package pageObjects.herokuapp;
+
+import pageObjects.baseObjects.BasePage;
+
+public class SortableDataTablesPage extends BasePage {
+}
---
+++ src/test/java/lecture_12/practice/SortableDataTablesTest.java
@@ -0,0 +1,18 @@
+package lecture_12.practice;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import pageObjects.baseObjects.BaseTest;
+import pageObjects.herokuapp.home.HomePage;
+
+public class SortableDataTablesTest extends BaseTest {
+
+    HomePage homePage;
+
+    @BeforeClass
+    public void preconditions(){
+
+
+    }
+
+}
