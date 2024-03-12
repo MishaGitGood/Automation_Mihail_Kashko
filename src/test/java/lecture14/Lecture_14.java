6a2826c819d94209606371d21c383b27d7bdbeee
Misha <m.kashko.21vek.by@gmail.com>
2024-03-12 12:21:57 +0300

    add: lecture14

---
+++ src/test/java/lecture14/Lecture_14.java
@@ -0,0 +1,17 @@
+package lecture14;
+
+import lecture14.entity.Car;
+import org.testng.annotations.Test;
+
+public class Lecture_14 {
+
+    @Test
+    public void test(){
+        Car car = new Car();
+        car.s
+
+
+
+    }
+
+}
---
+++ src/test/java/lecture14/entity/Car.java
@@ -0,0 +1,14 @@
+package lecture14.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Car {
+    private String model;
+    private String type;
+    private Integer year;
+    private Double engine;
+}
